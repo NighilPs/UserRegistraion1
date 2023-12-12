975358ace062b0791e3ef8fe729477d944383d37
NighilPs <nighilps123@gmail.com>
2023-12-12 23:33:54 +0530

    Checking User exist or not

--- crud/querydata.py
+++ crud/querydata.py
@@ -7,7 +7,7 @@ def get_user(db: Session):
     return db.query(User).all()
 
 def post_user(db:Session,full_name,password,email,phone):
-    email_exist = db.query(User.email == email).first()
+    email_exist = db.query(User).filter(User.email == email).first()
     if not email_exist:
         data = User(full_name = full_name,password = password,email=email,phone=phone)
         db.add(data)
